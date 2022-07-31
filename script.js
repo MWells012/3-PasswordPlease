aafdfaec9cfe5c8938de219b1e490883718f6bb7
Michaela Wells <michaelawells@rocketmortgage.com>
2022-07-31 17:08:13 -0400

    initial files added to folder

---
+++ script.js
@@ -0,0 +1,19 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+var numericalChar = "0123456789";
+var specialChar = "!#$%&?@";
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button -> button click
+generateBtn.addEventListener("click", writePassword);
