19125999d402d64f1a6a59c4502d9946401d6276
Michaela Wells <michaelawells@rocketmortgage.com>
2022-07-31 19:03:41 -0400

    another attempt at writing the javascript. hasn't been spitting out an actual usable password

--- script.js
+++ script.js
@@ -1,60 +1,51 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
-var numericalChar = "0123456789";
-var specialChar = "!#$%&?@";
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-passwordLengthUser = parseInt(passwordLengthUser);
-if (passwordLengthUser < 8) {
-    alert("Password must have more than 7 characters!");
-return "";
-}
-if (passwordLengthUser > 128) {
-    alert("Password much not have more than 128 characters!");
-return ""
-}
-var lowercaseCharactersChoice = confirm("Sprinkle in some lowercase characters?");
-if (lowercaseCharactersChoice) {
-passwordChar += lowercaseChar;
-}
-
-var uppercaseCharactersChoice = confirm("How about a few uppercase letters?");
-if (uppercaseCharactersChoice) {
-passwordChar += uppercaseChar;
-}
-
-var numericalCharactersChoice = confirm("What’s a password without a couple of numbers? Would you like to add them to yours?");
-if (numericalCharactersChoice) {
-passwordChar += numericalChar;
-}
-
-var specialCharacterChoice = confirm("Your password is pretty secure already, but how about a dash of special characters?");
-if (specialCharacterChoice) {
-passwordChar += specialChar;
-}
-
-for (var i = 0; i < passwordLengthUser; i++) {
-password = passwordChar[Math.floor(Math.random() * passwordChar.length)]
-}
-
-function writePassword(password){
-//var password = generatePassword();
-var pwTextArea = document.getElementById("#password");
-pwTextArea.value = password;
-return ("")
-
-}
-console.log(writePassword)
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button -> button click
 generateBtn.addEventListener("click", writePassword);
+
+function writePassword() {
+  // creates user prompt to select password length
+  function writePassword() {
+    var passwordChars = "";
+    var passwordLength = 0;
+    var OptionLowerCase = false, OptionUpperCase = false, OptionNumeric = false, OptionSpecialCharacters = false;
+    passwordLength = prompt("Please enter the length you would like your password, must be between 8 and 128");
+
+    // verify password length
+    if (passwordLength >= 8 && passwordLength <= 128) {
+      OptionLowerCase = confirm("Do you want lowercase letters?");
+      OptionUpperCase = confirm("Do you want uppercase letters?");
+      OptionNumeric = confirm("Do you want numbers?");
+      OptionSpecialCharacters = confirm("Do you want special characters? (!@#$%&?)");
+
+    // verify at least one char type
+    if (OptionLowerCase || OptionUpperCase || OptionNumeric || OptionSpecialCharacters) {
+      return passwordChars = createPassword(passwordLength, [OptionLowerCase, OptionUpperCase, OptionNumeric, OptionSpecialCharacters]);
+    }
+    else {
+      alert("You need to pick at least one character type.");
+    }
+  }  
+else {
+  alert("Invalid password length.");
+}
+  
+}
+
+function createPassword(pLength, pOptions) {
+  var generatedPassword = "";
+  const lowerCaseSet = "abcdefghijklmnopqrstuvwxyz";
+  const upperCaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numbersSet = "0123456789";
+  const specialCharsSet = "!@#$%&?";
+}
+}
